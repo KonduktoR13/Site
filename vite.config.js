5d73c3d5d2b2ed6a946882c4c0e942f5b80068b7
KonduktoR13 <budilnik53@gmail.com>
2025-09-07 19:46:52 +0300

    Configure GitHub Pages build and deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: './',
+  // GitHub Pages serves this site from https://<user>.github.io/Site/
+  // so all asset URLs must be prefixed with /Site/
+  base: '/Site/',
   plugins: [vue()]
 })
